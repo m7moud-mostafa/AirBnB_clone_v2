5e32f39d855ceca3fdc2833f0622cfa7ee5db31d
Mahmoud <mah2002moud@gmail.com>
2024-06-27 01:17:21 +0300

    Rokkia's place and user

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel, Base
 class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
