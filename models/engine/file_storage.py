fc63ca681f8e0fb817fe4c002efe35f88f35beb0
Mahmoud <mah2002moud@gmail.com>
2024-06-27 00:55:04 +0300

    Rokkia's spaghetti

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+
+    __file_path = 'fiii.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
         if cls is None:
             return self.__objects
         if type(cls) == str:
@@ -17,19 +21,15 @@ class FileStorage:
         filtered_objects = {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
         return filtered_objects
 
-
     def new(self, obj):
-        """Adds new object to storage dictionary"""
         self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
         json_objects = {k: v.to_dict() for k, v in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
         try:
             with open(self.__file_path, 'r') as f:
                 json_objects = json.load(f)
@@ -40,10 +40,9 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-def delete(self, obj=None):
-    """deletes ///"""
-    if obj is None:
-        return
-    key = obj.__class__.__name__ + '.' + obj.id
-    if key in self.__objects:
-        del self.__objects[key]
+    def delete(self, obj=None):
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.__objects:
+            del self.__objects[key]
